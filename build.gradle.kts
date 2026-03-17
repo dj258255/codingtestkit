9d2adc196391b2f6e3f4dd01804417ec4af71e18
범수 <dj258255@naver.com>
2026-03-17 19:25:10 +0900

    fix: build.gradle.kts 버전을 1.2.1로 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.codingtestkit"
-version = "1.2.0"
+version = "1.2.1"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ intellijPlatform {
     pluginConfiguration {
         id = "com.codingtestkit"
         name = "CodingTestKit"
-        version = "1.2.0"
+        version = "1.2.1"
         ideaVersion {
             sinceBuild = "241"
             untilBuild = "263.*"
